f552370db9daf5ab472881fd02b5d1a047ccbdd2
Juan Bautista Blasco <juan.blasco@globant.com>
2016-06-25 17:50:44 -0300

    injection on routes

--- src/components/app/services/api.js
+++ src/components/app/services/api.js
@@ -3,7 +3,6 @@ import {bands} from './mock-data';
 export class ApiService {
     url = 'http://localhost:1337/';
 
-
     constructor ($http) {
         this.http = $http;
     }
@@ -11,6 +10,26 @@ export class ApiService {
     getBands() {
     	console.log("llamada a get bands");
     	return new Promise(resolve => resolve(bands));
+    }
+
+    getAlbums(bandId) {
+    	console.log("llamada a get albums");
+    	return new Promise(resolve => resolve(albums));
+    }
+
+    getArtists(bandId) {
+    	console.log("llamada a get artists");
+    	return new Promise(resolve => resolve(artists));
+    }
+
+    getTracks(albumId) {
+    	console.log("llamada a get tracks");
+    	return new Promise(resolve => resolve(tracks));
+    }
+
+    getComments(trackId) {
+    	console.log("llamada a get comments");
+    	return new Promise(resolve => resolve(comments));
     }
 
 }
